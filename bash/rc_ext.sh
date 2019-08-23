d67d571cb5a1068198253113859d3ee4ab3b56a2
Everett Wilson <everettwilson@pop-os.localdomain>
2019-08-23 02:57:17 -0500

    Lump bash customizations into extension file to be loaded separately, solidify move back to bash form fish

---
+++ bash/rc_ext.sh
@@ -0,0 +1,44 @@
+# My wonderful little preferences, aliases, functions, etc, to make my shell feel like home
+# Add this line to the end of .bashrc to load it in:
+# [ -s "${HOME}/.dotfiles/bash/rc_ext.sh" ] && \. "${HOME}/.dotfiles/bash/rc_ext.sh"
+
+# Colors to see dirs and bins
+LS_COLORS="di=34:ex=33"
+export $LS_COLORS
+
+# Function to auto-dial into a tmux session over SSH
+# Usage: tsh <[username@]hostname> <tmux-session>
+function tsh {
+    host=$1
+    session_name=$2
+    if [ -z "$session_name" ]; then
+        echo "Need to provide session-name. Example: tsh <hostname> <tmux-session>"
+        return 1;
+    fi
+    ssh -X $host -t "tmux -CC attach -t $session_name || tmux -CC new -s $session_name"
+}
+
+# Set custom prompt
+function abbreviated_pwd {
+    ps_str=''
+    pwd_str=$(pwd)
+    if [ ! -z $(pwd | grep "^${HOME}") ]; then
+        pwd_str=$(echo $pwd_str | sed "s#${HOME}#~#")
+    fi
+    OLD_IFS=$IFS
+    IFS='/' # set delimiter
+    read -ra ADDR <<< ${pwd_str} # pwd output is read into an array as tokens separated by IFS
+    for i in "${ADDR[@]}"; do # access each element of array
+        ps_str+="${i:0:1}/"
+    done
+    IFS=${OLD_IFS} # reset to default value after usage
+    echo ${ps_str}
+}
+PS1='\[\e[1;32m\]\u@\h:\[\e[1;34m\]$(eval abbreviated_pwd)\[\e[0m\]$ '
+
+# Adjust FZF to use ripgrep
+export FZF_DEFAULT_COMMAND='rg --files --hidden -g "!{.git,node_modules}/*"'
+# Insert file from fzf into command
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+# Edit file from fzf
+bind -x '"\C-p": nvim $(fzf);'
