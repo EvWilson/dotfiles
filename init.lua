64f3a4bc1da9af3004bd01adbb5bb02612a05b8d
Everett Wilson <everett.wilson@validic.com>
2023-05-24 14:01:45 -0500

    Fix organize imports on save for go

--- init.lua
+++ init.lua
@@ -133,9 +133,14 @@ require("lazy").setup({
           async = false,
           timeout_ms = 10000,
         },
-        servers = {
-          ['goimports'] = { 'go' },
-        }
+      })
+
+      -- Taken from: https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.go',
+        callback = function()
+          vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+        end
       })
 
       -- (Optional) Configure lua language server for neovim
