3c11308d46bd7fa15cd4a98e4c3810bcc83c6a2d
Everett Wilson <everett.wilson@validic.com>
2023-06-08 16:51:36 -0500

    Update slimux target

--- init.lua
+++ init.lua
@@ -228,7 +228,7 @@ require("lazy").setup({
       local slimux = require('slimux')
       slimux.setup({
         target_socket = slimux.get_tmux_socket(),
-        target_pane = string.format('%s.1', slimux.get_tmux_window()),
+        target_pane = string.format('%s.2', slimux.get_tmux_window()),
       })
       vim.keymap.set('v', '<leader>r', ':lua require("slimux").send_highlighted_text()<CR>',
         { desc = 'Send currently highlighted text to configured tmux pane' })
