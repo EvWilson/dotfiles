8c9fa0a1499b90bcd12bcb8f33a989a95649a9cd
EvWilson <everett.wilson@runzero.com>
2025-06-05 09:26:46 -0500

    Woops, switch off local spelunk

--- init.lua
+++ init.lua
@@ -492,8 +492,8 @@ require("lazy").setup({
 		end,
 	},
 	{
-		-- "EvWilson/spelunk.nvim",
-		dir = "~/Documents/spelunk.nvim",
+		"EvWilson/spelunk.nvim",
+		-- dir = "~/Documents/spelunk.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope.nvim",
